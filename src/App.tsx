61e1e212729a21015f035b808d7a477d5cf1050a
zhongyongsheng <itvincent@gmail.com>
2022-07-19 15:18:13 +0800

    feat(interface):add sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+// 测试函数类型 function type
+interface FunctionInterface<A, B> {
+  (value: A): B;
+  (value: A, type: string): B;
+}
+
+const func: FunctionInterface<string, number> = function (value): number {
+  return +value
+}
+
+// 类型接口
+interface TypeInterface {
+  key: number;
+  value: string;
+  option?: string;
+}
+
+interface SubTypeInterface extends TypeInterface {
+  code: number;
+}
+
+// 数组接口
+interface ArrayInterface {
+  [index: number]: number;
+}
+
+
 function App() {
+  const type: TypeInterface = { key: 1, value: "123", option: "doc" }
+  const subType: SubTypeInterface = { key: 1, value: "123", option: "doc", code: 100 }
+  const nums: ArrayInterface = [1, 2, 3, 4]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {func("1")}
+      <p />
+      {typeof (func("1"))}
+      <p />
+      {type.key + ":" + type.value + "~" + type.option}
+      <p />
+      {subType.key + ":" + subType.value + "~" + subType.option + "-" + subType.code}
+      <p />
+      {nums}
+    </>
   );
 }
 
